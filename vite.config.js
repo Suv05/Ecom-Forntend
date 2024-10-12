c1f4356cdf81e21045b30efeb484a29c867b915b
suvam <suvampravu@gmail.com>
2024-10-12 22:39:07 +0530

    commit changes

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-    build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-  }
-
-})
+  build: {
+    outDir: "dist", // Ensure this is set to 'dist'
+  },
+});
